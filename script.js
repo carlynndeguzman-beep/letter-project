407cb4a5eca3de9493e39ec48be281aed73fbe51
carlynndeguzman-beep <carlynndeguzman@gmail.com>
2026-03-01 10:13:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `HI BABYYY I KNOW YOU'RE NOT OKAYY.\n SO if you do… I just want you to know, I think about you EVERYTIME.\n\nI hope you know that i'm
+always here. You're not alone. you will always have me in every universe. please don't give up hm? papakasalan pa kita. its alright to feel tired baby but dont give up.\n\nJust to let you know, wife, i wont get tired of being by yourside everyday, with you i feel so complete. ako nagsasabi na hindi totoo sinasabi nila.\n\nAnyway, I hope you're doing okay, even though ganon sinasabi nila sayo, i love you to the moon and back wife.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -17,7 +18,7 @@ function showLetter() {
         setTimeout(typeWriter, 30);
       }
     }
-
     typeWriter();
   }, 600);
 }
+
